adec92793d35480cfd65bdf3c0f85b50185af70b
Iván Pacheco <ivanpsmx@msn.com>
2019-01-16 13:48:12 -0700

    Update customTests.R
    Change base64encode by base64.

--- Basic_Building_Blocks/customTests.R
+++ Basic_Building_Blocks/customTests.R
@@ -35,6 +35,6 @@ submit_log <- function(){
                         datetime = p(log_$datetime, nrow_, NA),
                         stringsAsFactors = FALSE)
   write.csv(log_tbl, file = temp, row.names = FALSE)
-  encoded_log <- base64encode(temp)
+  encoded_log <- base64(temp)
   browseURL(paste0(pre_fill_link, encoded_log))
 }
